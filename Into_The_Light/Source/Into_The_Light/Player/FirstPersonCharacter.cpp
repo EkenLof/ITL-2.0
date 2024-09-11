7c0cc9c36f768de418f4d2211265e0faba175293
Jonathan Ek <ekenlof@hotmail.com>
2024-09-12 00:08:03 +0200

    Fixes

--- Into_The_Light/Source/Into_The_Light/Player/FirstPersonCharacter.cpp
+++ Into_The_Light/Source/Into_The_Light/Player/FirstPersonCharacter.cpp
@@ -473,6 +473,9 @@ void AFirstPersonCharacter::Tick(float DeltaTime)
 		if (IsValid(ExitReceptionPhoneTrigActor)) ExitReceptionPhoneTrigActor->SetActorEnableCollision(true);
 		else UE_LOG(LogTemp, Warning, TEXT("ExitReceptionPhoneActor is NOT Valid"));
 
+		// Load Lights F2
+		LoadSublevel(TEXT("LightsF2")); // 11-09-2024
+
 		bIsReceptionPhone = false;
 	}
 	// Fuse 16a to Box // bIsFuse16a = Fuse16a Selected
